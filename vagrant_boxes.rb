ab8338bfc89287a96e1e68fb51959193637872ab
Stefano Zanella <zanella.stefano@gmail.com>
2013-07-02 00:28:07 +0200

    Add basic index page with link to CentOS base box.

--- vagrant_boxes.rb
+++ vagrant_boxes.rb
@@ -1,9 +1,25 @@
 require 'sinatra/base'
+require 'slim'
+require 'pathname'
 
 module Derecom
   class VagrantBoxes < Sinatra::Base
     get '/' do
-      "This is de.re.com Vagrant boxes repository"
+      slim :index
+    end
+    
+    get '/boxes/:box' do
+      send_file path_for(params[:box])
+    end
+
+    private
+
+    def path_for(box)
+      data_dir + box
+    end
+
+    def data_dir
+      @data_dir ||= Pathname.new(ENV['OPENSHIFT_DATA_DIR'])
     end
   end
 end
